9a2f1bc4bff29ead9ea0efd3e249f8560b291873
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-04-16 13:49:26 -0400

    added event listeners to divs

--- game.js
+++ game.js
@@ -1,25 +1,37 @@
+console.log('connected')
+
+//select colored buttons
+const greenDiv = document.querySelector(".green");
+const redDiv = document.querySelector(".red");
+const yellowDiv = document.querySelector(".yellow");
+const blueDiv = document.querySelector(".blue");
+
 //function to toggle green light
 function greenLight() {
-    greenDiv = document.querySelector("green");
-    greenDiv.classList.toggle("green-light");
+    console.log('clicked green')
+    greenDiv.classList.toggle("green-light")
   }
 
 //function to toggle red light
 function redLight() {
-    redDiv = document.querySelector("red");
+    console.log('clicked red')
     redDiv.classList.toggle("red-light");
   }
 
 //function to toggle yellow light
 function yellowLight() {
-    yellowDiv = document.querySelector("yellow");
+    console.log('clicked yellow')
     yellowDiv.classList.toggle("yellow-light");
   }
 
 //function to toggle blue light
 function blueLight() {
-    blueDiv = document.querySelector("blue");
+    console.log('clicked blue')
     blueDiv.classList.toggle("blue-light");
   }
 
- 
+//toggle light class when color divs are clicked
+ greenDiv.addEventListener('click', greenLight)
+ redDiv.addEventListener('click', redLight)
+ yellowDiv.addEventListener('click', yellowLight)
+ blueDiv.addEventListener('click', blueLight)
