bdfb1e67ecf8d2ce0921445a32d8c492bcf9d26f
Gabriella De Francesco <gabriella.defrancesco92@gmail.com>
2019-04-16 13:40:19 -0400

    add light functions

---
+++ game.js
@@ -0,0 +1,25 @@
+//function to toggle green light
+function greenLight() {
+    greenDiv = document.querySelector("green");
+    greenDiv.classList.toggle("green-light");
+  }
+
+//function to toggle red light
+function redLight() {
+    redDiv = document.querySelector("red");
+    redDiv.classList.toggle("red-light");
+  }
+
+//function to toggle yellow light
+function yellowLight() {
+    yellowDiv = document.querySelector("yellow");
+    yellowDiv.classList.toggle("yellow-light");
+  }
+
+//function to toggle blue light
+function blueLight() {
+    blueDiv = document.querySelector("blue");
+    blueDiv.classList.toggle("blue-light");
+  }
+
+ 
